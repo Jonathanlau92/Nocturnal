1726da362b936bb7961a98ad36258dff90ad5872
Jonathanlau92 <jonathan@excide.co>
2020-08-08 22:44:56 +0800

    generate controllers

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'users/registrations', omniauth_callbacks: 'users/omniauth_callbacks' }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users
+  resources :league
   get "mission", to: "pages#mission"
   
   root to: "pages#home"
