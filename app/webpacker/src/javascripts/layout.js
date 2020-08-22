f9f58a11e747bc1460994ef431ab535709950d92
Jonathanlau92 <jonathan@excide.co>
2020-08-22 16:32:45 +0800

    Change px width to percentage

--- app/webpacker/src/javascripts/layout.js
+++ app/webpacker/src/javascripts/layout.js
@@ -1,4 +1,5 @@
 $(document).scroll(function() {
+	console.log("HEYYO: ", $('.container').hasClass('league-container'))
   if ($('.container').hasClass('league-container')) {
     if ($(document).scrollTop() > 30) {
       $('.main-header').addClass('d-none');
