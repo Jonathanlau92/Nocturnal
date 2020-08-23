42a10616bc8615862b762de755dca159bac6c13a
Jonathanlau92 <jonathan@excide.co>
2020-08-23 09:55:30 +0800

    Remove user reference from images

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_23_011848) do
+ActiveRecord::Schema.define(version: 2020_08_23_015448) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,12 +37,10 @@ ActiveRecord::Schema.define(version: 2020_08_23_011848) do
   end
 
   create_table "images", force: :cascade do |t|
-    t.bigint "user_id", null: false
     t.integer "image_category"
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["user_id"], name: "index_images_on_user_id"
   end
 
   create_table "leagues", force: :cascade do |t|
@@ -106,7 +104,6 @@ ActiveRecord::Schema.define(version: 2020_08_23_011848) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "images", "users"
   add_foreign_key "matches", "leagues"
   add_foreign_key "matches", "teams"
   add_foreign_key "team_details", "teams"
