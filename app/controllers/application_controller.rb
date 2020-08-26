979a86f32fb872564f29112a894f46c205c1f598
Jonathanlau92 <jonathan@excide.co>
2020-08-26 22:28:40 +0800

    Add superadmin status to user for control access to admin page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,12 @@ class ApplicationController < ActionController::Base
       stored_location_for(resource) || root_path
     end
   end
+
+  def authenticate_active_admin_user!
+    authenticate_user!
+    unless current_user.superadmin?
+      flash[:alert] = "Unauthorized Access!"
+      redirect_to root_path
+    end
+  end
 end
