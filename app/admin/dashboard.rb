7cf7b8fe0f7ea6ed9bfd330a5d21dd2920ffc445
Jonathanlau92 <jonathan@excide.co>
2020-08-27 19:25:15 +0800

    Add simple dashboard

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -11,16 +11,17 @@ ActiveAdmin.register_page "Dashboard" do
 
     # Here is an example of a simple dashboard with columns and panels.
     #
-    # columns do
-    #   column do
-    #     panel "Recent Posts" do
-    #       ul do
-    #         Post.recent(5).map do |post|
-    #           li link_to(post.title, admin_post_path(post))
-    #         end
-    #       end
-    #     end
-    #   end
+    columns do
+      column do
+        panel "Recent Leagues" do
+          ul do
+            League.all.each do |league|
+              li link_to(league.title, admin_league_path(league))
+            end
+          end
+        end
+      end
+    end
 
     #   column do
     #     panel "Info" do
@@ -28,5 +29,5 @@ ActiveAdmin.register_page "Dashboard" do
     #     end
     #   end
     # end
-  end # content
+  end
 end
