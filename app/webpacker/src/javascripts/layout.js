554bc4aebc8fbb6384f35ffb6b2cfead363e1ec5
Jonathanlau92 <jonathan@excide.co>
2020-08-29 15:25:09 +0800

    Fix glitches when hiding navbar

--- app/webpacker/src/javascripts/layout.js
+++ app/webpacker/src/javascripts/layout.js
@@ -1,12 +1,40 @@
-$(document).scroll(function() {
-	console.log("HEYYO: ", $('.container').hasClass('league-container'))
+$(document).on("turbolinks:load", function() { 
   if ($('.container').hasClass('league-container')) {
-    if ($(document).scrollTop() > 30) {
-      $('.main-header').addClass('d-none');
-    }
-    else {
-      $('.main-header').removeClass('d-none');
+    var didScroll;
+    var lastScrollTop = 0;
+    var delta = 5;
+    var navbarHeight = $('.main-header').outerHeight();
+
+    $(window).scroll(function(event){
+      didScroll = true;
+    });
+
+    setInterval(function() {
+      if (didScroll) {
+          hasScrolled();
+          didScroll = false;
+      }
+    }, 250);
+
+    function hasScrolled() {
+      var st = $(this).scrollTop();
+      
+      // Make sure they scroll more than delta
+      if(Math.abs(lastScrollTop - st) <= delta)
+          return;
+      
+      // If they scrolled down and are past the navbar, add class .nav-up.
+      // This is necessary so you never see what is "behind" the navbar.
+      if (st > lastScrollTop && st > navbarHeight){
+        // Scroll Down
+        $('.main-header').removeClass('nav-down').addClass('nav-up');
+      } else {
+        // Scroll Up
+        if(st + $(window).height() < $(document).height()) {
+          $('.main-header').removeClass('nav-up').addClass('nav-down');
+        }
+      }
+      lastScrollTop = st;
     }
   }
 });
-
