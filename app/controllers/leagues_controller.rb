4664c4c349705ecfa319f6358804a8782a562bd3
Jonathanlau92 <jonathan@excide.co>
2020-10-10 23:06:13 +0800

    Fix league controller spacing

--- app/controllers/leagues_controller.rb
+++ app/controllers/leagues_controller.rb
@@ -1,21 +1,21 @@
 class LeaguesController < ApplicationController
-	def index
-		
-	end
+  def index
+    
+  end
 
-	def team_details
-		
-	end
+  def team_details
+    
+  end
 
-	def participants
+  def participants
     if params[:team].present?
       @teams = Team.all
     elsif params[:sort] == 'true'
       # LOWER to make it non-case sensitive
       @users = User.order('LOWER(username) ASC')
     else
-		  @users = User.all
+      @users = User.all
     end
     @default_image = Image.first
-	end
+  end
 end
