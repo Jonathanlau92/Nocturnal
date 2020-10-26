6e0284ffd943f2098deee554c98f4f905682a418
LimNing <e0324216@u.nus.edu>
2020-10-27 02:06:56 +0800

    Change production env smtp domain to heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,7 +72,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     :user_name => ENV['SENDGRID_USERNAME'],
     :password => ENV['SENDGRID_PASSWORD'],
-    :domain => 'http://nocturnal-staging.herokuapp.com',
+    :domain => 'heroku.com',
     :address => 'smtp.sendgrid.net',
     :port => 587,
     :authentication => :plain,
