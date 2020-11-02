efdfffd0439fd02ac63c9af31bc6875a04ac28f6
Jonathanlau92 <jonathan@excide.co>
2020-11-02 22:18:54 +0800

    Update active admin panels

--- app/admin/heros.rb
+++ app/admin/heros.rb
@@ -1,16 +1,16 @@
-ActiveAdmin.register League do
+ActiveAdmin.register Hero do
 
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
   # Uncomment all parameters which should be permitted for assignment
   #
-  permit_params :title, :date, :price, :moderator, :lobby_id, :lobby_password, :discord_link
+  permit_params :name, :hero_identifier
   #
   # or
   #
   # permit_params do
-  #   permitted = [:title, :date, :price]
+  #   permitted = [:name, :hero_identifier]
   #   permitted << :other if params[:action] == 'create' && current_user.admin?
   #   permitted
   # end
