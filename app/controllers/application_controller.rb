421f9d0de521fa0d9e65e8847e6f8f6ad274a8db
Jonathanlau92 <jonathan@excide.co>
2020-11-07 10:51:11 +0800

    Fix signing up with steam to never re-logging again

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,10 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  protect_from_forgery prepend: true
   
   def after_sign_in_path_for(resource)
-    if resource.steam_authentication_data.present? and resource.email.empty?
-      edit_user_path(current_user)
-    else
-      stored_location_for(resource) || root_path
-    end
+    stored_location_for(resource) || root_path
   end
 
   def authenticate_active_admin_user!
