b8c947380838bbe652e946e439a426ae511127bf
Jonathanlau92 <jonathan@excide.co>
2020-11-07 12:29:19 +0800

    Validate password strength

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable
+         :recoverable, :rememberable, :validatable
   devise :omniauthable, omniauth_providers: %i[steam]
  
   has_many :user_teams
@@ -26,6 +26,8 @@ class User < ApplicationRecord
       transitions from: :purchased_pass, to: :no_pass
     end
   end
+
+  validate :password_regex
 
   after_create :send_welcome_mail
 
@@ -66,5 +68,12 @@ class User < ApplicationRecord
 
   def send_welcome_mail
     UserMailer.with(user: self).welcome.deliver_later
+  end
+
+  private
+
+  def password_regex
+    return if password.blank? || password =~ /\A(?=.*\d)(?=.*[A-Z])(?=.*\W)[^ ]{7,}\z/
+    errors.add :password, 'Password should have more than 7 characters including 1 uppercase letter, 1 number, 1 special character'
   end
 end
