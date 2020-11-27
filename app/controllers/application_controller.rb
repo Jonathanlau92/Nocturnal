97316b144c01b4435ebac8a28ed97720482ff684
Jonathanlau92 <jonathan@excide.co>
2020-11-27 23:04:10 +0800

    Redirect user to connect steam when sign in for the first time

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,11 @@ class ApplicationController < ActionController::Base
   protect_from_forgery prepend: true
   
   def after_sign_in_path_for(resource)
-    stored_location_for(resource) || root_path
+    if resource.steam_authentication_data.present?
+      stored_location_for(resource) || root_path
+    else
+      edit_user_path(resource)
+    end
   end
 
   def authenticate_active_admin_user!
